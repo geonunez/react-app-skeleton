d9d5d83d856654789403286f77e730011701f873
geonunez <geonunez@gmail.com>
2017-12-28 12:11:18 -0300

    Added react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Router, Route, Switch } from 'react-router-dom';
 
-import logo from '../logo.svg';
-import '../styles/App.css';
+import 'semantic-ui-css/semantic.min.css';
+import '../css/App.css';
+
+import { history } from '../helpers';
+
+import Main from './Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={ history }>
+        <Switch>
+          <Route exact path='/' component={ Main } />
+        </Switch>
+      </Router>
     );
   }
 }
