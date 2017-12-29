f87e2377fa6cafe91da74a064599717a1ba4378a
geonunez <geonunez@gmail.com>
2017-12-29 15:35:06 -0300

    Applied that do not logger redux when env is equal to PROD

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,8 +1,10 @@
 import config from 'react-global-configuration';
 
-const backendDomain = process.env.REACT_APP_BACKEND_DOMAIN || '/';
+const env = process.env.REACT_APP_ENV || 'DEV',
+  backendDomain = process.env.REACT_APP_BACKEND_DOMAIN || '/';
 
 config.set({
+  env: env.toUpperCase(),
   backend_domain: backendDomain,
   api_user_login: backendDomain + '/api/v1/users/login',
 });
--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,10 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
+import config from 'react-global-configuration';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers/rootReducer';
 
+let middlewares = [
+    thunk
+]
+
+if (config.get('env') !== 'PROD') {
+    middlewares = [...middlewares, logger];
+}
+
 export const store = createStore(
     rootReducer,
-    applyMiddleware(logger, thunk)
+    applyMiddleware(...middlewares)
 );
